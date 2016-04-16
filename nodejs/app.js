7cdd8d5be428ecf9be5143f5a3da9ef3d45d0892
Patrick <patrickedelen@gmail.com>
2016-04-15 22:46:42 -0500

    Worked more on modules

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,23 +1,3 @@
 var greet = require('./greet.js'); //.js extension isn't required
 
-var person = {
-	firstname: 'John',
-	lastname: 'Doe',
-	greet: function() {
-		console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-	}
-};
-
-function Person(firstname, lastname) {
-	this.firstname = firstname;
-	this.lastname = lastname;
-}
-
-Person.prototype.greet = function() {
-	console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-};
-
-var john = new Person('John', 'Doe');
-john.greet();
-
-console.log(john.__proto__);//do not keep
+greet();
--- nodejs/greet.js
+++ nodejs/greet.js
@@ -1,5 +1,6 @@
 var greet = function() {
 	console.log('Hello!');
+	console.log('This is a greeting!')
 };
 
 module.exports = greet;
